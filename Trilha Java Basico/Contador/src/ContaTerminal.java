14904edc5252df1a7772fc4ade565a71799f7d61
Natan Sales <victornatansalles@gmail.com>
2024-05-23 21:55:54 -0300

    Desafio Controle de Fluxo

--- Trilha Java Basico/Contador/src/ContaTerminal.java
+++ Trilha Java Basico/Contador/src/ContaTerminal.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+	public static void main(String[] args) throws Exception {
+		Scanner in = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite o número da Conta:");
-        int numero = in.nextInt();
-        in.nextLine();
+		System.out.println("Digite o número da Conta:");
+		int numero = in.nextInt();
+		in.nextLine();
 
-        System.out.println("Digite o número da Agência:");
-        String agencia = in.nextLine();
+		System.out.println("Digite o número da Agência:");
+		String agencia = in.nextLine();
 
-        System.out.println("Digite o seu Nome:");
-        String nome = in.nextLine();
+		System.out.println("Digite o seu Nome:");
+		String nome = in.nextLine();
 
-        System.out.println("Digite o seu Saldo:");
-        double saldo = in.nextDouble();
+		System.out.println("Digite o seu Saldo:");
+		double saldo = in.nextDouble();
 
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
-                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+		System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+				+ ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
-        in.close();
-    }
+		in.close();
+	}
 }
