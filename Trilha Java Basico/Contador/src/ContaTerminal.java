8b0930d9ab2ad03090f19389d579f8f51088acc6
Natan Sales <victornatansalles@gmail.com>
2024-05-23 21:45:12 -0300

    Desafio Trilha Java Básico

---
+++ Trilha Java Basico/Contador/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da Conta:");
+        int numero = in.nextInt();
+        in.nextLine();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = in.nextLine();
+
+        System.out.println("Digite o seu Nome:");
+        String nome = in.nextLine();
+
+        System.out.println("Digite o seu Saldo:");
+        double saldo = in.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+                + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        in.close();
+    }
+}
